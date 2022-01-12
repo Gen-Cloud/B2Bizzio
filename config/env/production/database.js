cb253ece4b918ecc7c81b90317fab6280ac7ed2c
Gen Cloud <gencloud.bg@gmail.com>
2022-01-12 09:00:19 +0200

    UPD: Database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,5 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+//const parse = require("pg-connection-string").parse;
+//const config = parse(process.env.DATABASE_URL);
 
 module.exports = () => ({
   defaultConnection: "default",
@@ -8,11 +8,11 @@ module.exports = () => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: ${db-postgres.HOSTNAME},
+        port: ${db-postgres.PORT},
+        database: ${db-postgres.DATABASE},
+        username: ${db-postgres.USERNAME},
+        password: ${db-postgres.PASSWORD},
         ssl: {
           rejectUnauthorized: false,
         },
@@ -21,3 +21,18 @@ module.exports = () => ({
     },
   },
 });
+//postgres://${db-postgres.USERNAME}:${db-postgres.PASSWORD}@${db-postgres.HOSTNAME}:${db-postgres.PORT}/${db-postgres.DATABASE}
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "b2bizzio"),
+//       user: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD", "h0db!zz1o"),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//   },
+// });
