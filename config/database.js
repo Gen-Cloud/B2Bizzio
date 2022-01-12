f572973fd452ccc02d88753dd93949de5ca74958
Gen Cloud <gencloud.bg@gmail.com>
2022-01-12 09:35:10 +0200

    UPD: Databases

--- config/database.js
+++ config/database.js
@@ -1,19 +1,13 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: {
-          ca: env("DATABASE_CA", ""),
-        },
-      },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "b2bizzio"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "h0db!zz1o"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,38 +1,19 @@
-//const parse = require("pg-connection-string").parse;
-//const config = parse(process.env.DATABASE_URL);
-
-module.exports = () => ({
+module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: "db-postgres-do-user-211953-0.b.db.ondigitalocean.com",
-        port: "25060",
-        database: "defaultdb",
-        username: "doadmin",
-        password: "PHLXMyWRhDzwJw8J",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
         ssl: {
-          rejectUnauthorized: false,
+          ca: env("DATABASE_CA", ""),
         },
       },
-      options: {},
     },
   },
 });
-//postgres://${db-postgres.USERNAME}:${db-postgres.PASSWORD}@${db-postgres.HOSTNAME}:${db-postgres.PORT}/${db-postgres.DATABASE}
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME", "b2bizzio"),
-//       user: env("DATABASE_USERNAME", "postgres"),
-//       password: env("DATABASE_PASSWORD", "h0db!zz1o"),
-//       ssl: env.bool("DATABASE_SSL", false),
-//     },
-//   },
-// });
