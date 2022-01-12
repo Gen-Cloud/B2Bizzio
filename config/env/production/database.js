05c8dc4b43def9199860574ffb60af2275280adb
Gen Cloud <gencloud.bg@gmail.com>
2022-01-12 10:05:26 +0200

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,9 +10,7 @@ module.exports = ({ env }) => ({
         database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: {
-          ca: env("DATABASE_CA", ""),
-        },
+        ssl: true,
       },
     },
   },
